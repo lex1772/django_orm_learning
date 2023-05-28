5e119be2f0f44eb9bded12b75c63de1edc549a9b
lexa1 <lexa1772@gmail.com>
2023-05-28 21:19:07 +0300

    ДЗ Шаблонизация сделана пагинация, но не заполняется форма

--- main/forms.py
+++ main/forms.py
@@ -1,13 +1,19 @@
+from datetime import datetime
+
 from django.forms import forms, CharField, ImageField, IntegerField, DateTimeField, ModelForm
 
-from main.models import Category
+from main.models import Category, Product
 
 
 class ProductForms(forms.Form):
     product_name = CharField(max_length=100)
     description = CharField(max_length=100)
-    image = ImageField()
+    image = ImageField(required=False)
     category = CharField(max_length=100)
     price = IntegerField()
-    creation_date = DateTimeField()
-    last_modified = DateTimeField()
+    creation_date = datetime.now()
+    last_modified = datetime.now()
+    class Meta:
+        model = Product
+        fields = ["product_name", "description", "category", "price"]
+        category = Category
--- main/views.py
+++ main/views.py
@@ -3,8 +3,11 @@ from datetime import datetime
 
 from django.core import serializers
 from django.core.paginator import Paginator, EmptyPage, InvalidPage, PageNotAnInteger
+from django.db.transaction import commit
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_protect
 
 from main.models import Product, Contacts, Category
 from .forms import ProductForms
@@ -13,9 +16,23 @@ from .forms import ProductForms
 # Create your views here.
 def home(request):
     products = Product.objects.all()[len(Product.objects.all()) - 5:len(Product.objects.all())]
+    if request.POST:
+        form = ProductForms(request.POST, request.FILES)
+        if form.is_valid():
+            cd = form.cleaned_data
+            product = Product(cd)
+            product.save()
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(products, 1)
+    try:
+        page_obj = paginator.page(page_num)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
     for i in products:
         print(i)
-    return render(request, 'main/home.html', {'products': products})
+    return render(request, 'main/home.html', {'products': products, 'page_obj': page_obj, 'form': ProductForms})
 
 
 def contact(request):
@@ -27,30 +44,3 @@ def contact(request):
         message = request.POST.get('message')
         print(f'You have new message from {name}({email}): {message}')
     return render(request, 'main/contact.html', {'contacts': contacts})
-
-
-def index(request):
-    object_list = Product.objects.all()[len(Product.objects.all()) - 5:len(Product.objects.all())]
-    page_num = request.GET.get('page', 1)
-
-    paginator = Paginator(object_list, 1)
-
-    try:
-        page_obj = paginator.page(page_num)
-    except PageNotAnInteger:
-        page_obj = paginator.page(1)
-    except EmptyPage:
-        page_obj = paginator.page(paginator.num_pages)
-
-    return render(request, 'main/home.html', {'page_obj': page_obj})
-
-
-def add_product(request):
-    product_form = ProductForms(request.POST, request.FILES)
-    if product_form.is_valid():
-        form = product_form.save(commit=False)
-        form.save()
-    else:
-        product_form = ProductForms()
-
-    return render(request, 'main/home.html', {'product_form': product_form})
