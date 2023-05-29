d37769b90f8bafb09b4d848c40ac58f75516ba63
lexa1 <lexa1772@gmail.com>
2023-05-29 19:03:32 +0300

    ДЗ Шаблонизация переделано

--- main/forms.py
+++ main/forms.py
@@ -1,15 +1,19 @@
 from datetime import datetime
 
-from django.forms import forms, CharField, ImageField, IntegerField, DateTimeField, ModelForm
+from django.forms import forms, CharField, ImageField, IntegerField, DateTimeField, ModelForm, ChoiceField
 
 from main.models import Category, Product
 
 
 class ProductForms(forms.Form):
+    CHOICE = [
+        ("сыр", "сыр"),
+        ("хлеб", "хлеб")
+    ]
     product_name = CharField(max_length=100)
     description = CharField(max_length=100)
     image = ImageField(required=False)
-    category = CharField(max_length=100)
+    category = ChoiceField(choices=CHOICE)
     price = IntegerField()
     creation_date = datetime.now()
     last_modified = datetime.now()
--- main/views.py
+++ main/views.py
@@ -15,33 +15,37 @@ from .forms import ProductForms
 
 # Create your views here.
 def home(request):
-    products = Product.objects.all()[len(Product.objects.all()) - 5:len(Product.objects.all())]
-    if request.POST:
-        form = ProductForms(request.POST, request.FILES)
-        if form.is_valid():
-            cd = form.cleaned_data
-            product_name = cd.get('product_name')
-            description = cd.get('description')
-            image = cd.get('image')
-            category = cd.get('category').lower()
-            price = cd.get('price')
-            cat = Category.objects.get(category=category)
-            creation_date = datetime.now()
-            last_modified = datetime.now()
-            product = Product(product_name=product_name, description=description, image=image, category=cat,
-                              price=price, creation_date=creation_date, last_modified=last_modified)
-            product.save()
-    page_num = request.GET.get('page', 1)
-    paginator = Paginator(products, 1)
     try:
-        page_obj = paginator.page(page_num)
-    except PageNotAnInteger:
-        page_obj = paginator.page(1)
-    except EmptyPage:
-        page_obj = paginator.page(paginator.num_pages)
-    for i in products:
-        print(i)
-    return render(request, 'main/home.html', {'products': products, 'page_obj': page_obj, 'form': ProductForms})
+        products = Product.objects.all()[len(Product.objects.all()) - 5:len(Product.objects.all())]
+    except ValueError:
+        products = Product.objects.all()
+    finally:
+        if request.POST:
+            form = ProductForms(request.POST, request.FILES)
+            if form.is_valid():
+                cd = form.cleaned_data
+                product_name = cd.get('product_name')
+                description = cd.get('description')
+                image = cd.get('image')
+                category = cd.get('category').lower()
+                price = cd.get('price')
+                cat = Category.objects.get(category=category)
+                creation_date = datetime.now()
+                last_modified = datetime.now()
+                product = Product(product_name=product_name, description=description, image=image, category=cat,
+                              price=price, creation_date=creation_date, last_modified=last_modified)
+                product.save()
+        page_num = request.GET.get('page', 1)
+        paginator = Paginator(products, 1)
+        try:
+            page_obj = paginator.page(page_num)
+        except PageNotAnInteger:
+            page_obj = paginator.page(1)
+        except EmptyPage:
+            page_obj = paginator.page(paginator.num_pages)
+        for i in products:
+            print(i)
+        return render(request, 'main/home.html', {'products': products, 'page_obj': page_obj, 'form': ProductForms})
 
 
 def contact(request):
