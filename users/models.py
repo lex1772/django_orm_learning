c2fe146477cf79554ffd62756a274c53f6b66811
lexa1 <lexa1772@gmail.com>
2023-06-18 10:42:02 +0300

    ДЗ 22.1 задание со звездочкой под вопросом

---
+++ users/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import AbstractUser, User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from main.models import NULLABLE
+
+
+# Create your models here.
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+
+    email_verify = models.BooleanField(default=False)
+
+    phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    country = models.CharField(max_length=100, verbose_name='страна', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            User.objects.create(user=instance)
+            instance.profile.save()
---
+++ users/views.py
@@ -0,0 +1,86 @@
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.urls import reverse_lazy
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode
+from django.views import View
+from django.views.generic import CreateView, UpdateView
+
+from users.forms import UserRegisterForm, UserProfileForm
+from users.models import User
+
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.utils.http import urlsafe_base64_decode
+
+from users.tokens import email_verification_token
+
+
+# Create your views here.
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+
+            current_site = get_current_site(request)
+            subject = 'Activate Your MySite Account'
+            message = render_to_string('users/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': email_verification_token.make_token(user),
+            })
+            user.email_user(subject, message)
+
+            messages.success(request, ('Please Confirm your email to complete registration.'))
+
+            return redirect('login')
+
+        return render(request, self.template_name, {'form': form})
+
+
+class ProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    success_url = reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class ActivateAccount(View):
+    success_url = reverse_lazy('users:login')
+
+    def get(self, request, uidb64, token, *args, **kwargs):
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        if user is not None and email_verification_token.check_token(user, token):
+            user.is_active = True
+            user.profile.email_confirmed = True
+            user.save()
+            login(request, user)
+            messages.success(request, ('Your account have been confirmed.'))
+            return redirect('home')
+        else:
+            messages.warning(request, ('The confirmation link was invalid, possibly because it has already been used.'))
+            return redirect('home')
